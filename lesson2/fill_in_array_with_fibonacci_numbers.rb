f98af387c3d4489aaaaf8496e55c550900d48a6c
Vladimir Kattsov <kattsoff@gmail.com>
2020-12-07 11:17:03 +0300

    Use negative indices in array

--- lesson2/fill_in_array_with_fibonacci_numbers.rb
+++ lesson2/fill_in_array_with_fibonacci_numbers.rb
@@ -3,10 +3,8 @@
 # Заполнить массив числами фибоначчи до 100
 
 array = [0, 1]
-index = 1
 loop do
-  index += 1
-  new_value = array[index - 1] + array[index - 2]
+  new_value = array[-1] + array[-2]
   break if new_value > 100
 
   array.push(new_value)
